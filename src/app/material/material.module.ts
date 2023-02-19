fd4cac9c276dbbcf61f65121c02be61006f845aa
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-19 19:48:17 -0300

    fix card sizes and wrap

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 // Modulos de Material Angular
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatGridListModule,
   ]
 })
 export class MaterialModule { }
