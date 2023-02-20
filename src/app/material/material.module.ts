b7e2e6018a4123ec0eb778c1316d4257d02eba42
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-19 21:48:11 -0300

    pipe switchMap

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 // Modulos de Material Angular
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatGridListModule,
     MatIconModule,
     MatListModule,
+    MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatGridListModule,
   ]
 })
 export class MaterialModule { }
